e65c7740ecae753a932b9ca4b647035e23e1150e
LlucSerramitjana <lluc.serramitjana@estudiantat.upc.edu>
2023-05-03 09:56:07 +0200

    generar claus working

--- src/app/generarclaus/generarclaus.component.ts
+++ src/app/generarclaus/generarclaus.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface RsaKeys {
-  publicKey: string;
-  privateKey: string;
+  publicKey: {
+    e: string;
+    n: string;
+  };
+  privateKey: {
+    d: string;
+    n: string;
+  };
 }
 
 @Component({
@@ -12,19 +18,22 @@ interface RsaKeys {
   styleUrls: ['./generarclaus.component.css']
 })
 export class GenerarclausComponent {
-
   rsaKeys: RsaKeys | undefined;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   onGenerarclaus() {
     console.log('Generating RSA keys...');
-    this.http.get<RsaKeys>('http://localhost:3000/generate-rsa-keys').subscribe(data => {
-      console.log('RSA keys generated:');
-      console.log('Public key:', data.publicKey);
-      console.log('Private key:', data.privateKey);
-      this.rsaKeys = data;
-    });
+    this.http.get<RsaKeys>('http://localhost:3000/generate-rsa-keys').subscribe(
+      (data) => {
+        console.log('RSA keys generated:');
+        console.log('Public key:', data.publicKey);
+        console.log('Private key:', data.privateKey);
+        this.rsaKeys = data;
+      },
+      (error) => {
+        console.error('Error generating RSA keys:', error);
+      }
+    );
   }
-
 }
